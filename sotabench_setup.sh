dfe3128a42039f9f62750b39ddada9b1e550afb1
deepparrot <53057954+deepparrot@users.noreply.github.com>
2019-10-05 16:17:03 +0100

    Update sotabench_setup.sh

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -11,7 +11,9 @@ pip install -v --no-cache-dir ./
 cd ..
 
 git clone https://github.com/open-mmlab/mmdetection/
-
+cd mmdetection
+git checkout 6e48d28198ea5d6dcf6a9618fd8d2832b5eaf538
+cd ..
 # copy files
 cp -r src/reppoints_head/* mmdetection/mmdet/models/anchor_heads/
 cp -r src/reppoints_detector/* mmdetection/mmdet/models/detectors/
@@ -20,7 +22,7 @@ cp -r src/reppoints_assigner/* mmdetection/mmdet/core/bbox/assigners/
 
 # install mmdetection
 cd mmdetection
-git checkout 6e48d28198ea5d6dcf6a9618fd8d2832b5eaf538
+
 echo "Installing..."
 $PYTHON setup.py develop
 cd ..
